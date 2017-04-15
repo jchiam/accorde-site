76907fd39b270f6c86c7ac16a680a770ec37b662
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-04-15 15:15:07 +0800

    Split webpack config to dev and production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   context: __dirname,
-  entry: ['react-hot-loader/patch', './src/app.js'],
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -26,12 +26,24 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.ejs',
       inject: 'body'
-    })
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true,
+        sequences: true,
+        booleans: true
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
   ]
 };
