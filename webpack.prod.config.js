882761b3fc115ac036c9ed94d1ad2b842d408e17
Jonathan Chiam <dcsczj@nus.edu.sg>
2017-04-25 15:28:51 +0800

    Update webpack config to support scss

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,13 +2,14 @@ const webpack = require('webpack');
 const path = require('path');
 const DotenvPlugin = require('webpack-dotenv-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: __dirname,
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.[hash].js'
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -23,6 +24,13 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader', 'import-glob-loader']
+        })
       }
     ]
   },
