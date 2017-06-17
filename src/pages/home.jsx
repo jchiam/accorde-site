d40bd48816edd0b95dad96494a3da7635147b428
Jonathan Chiam <jczjchiam@gmail.com>
2017-06-17 09:42:09 +0800

    Implement home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        This is Accordé Guitar Ensemble!
+        <img className="logo" src={process.env.LOGO} alt="logo" />
       </div>
     );
   }
