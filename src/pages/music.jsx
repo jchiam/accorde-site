3a270aaf011293edab3f22db2be7028b40c4fc45
Jonathan Chiam <jczjchiam@gmail.com>
2017-06-17 23:46:46 +0800

    Control youtube playback by visibility

--- src/pages/music.jsx
+++ src/pages/music.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import VisibilitySensor from 'react-visibility-sensor';
 import Youtube from 'react-youtube';
 
 import { fetchRandomVideo } from 'actions/youtube';
 import DataStates from 'constants/dataStates';
 
 class MusicPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { player: null };
+  }
+
   componentDidMount() {
     const { fetchVideo } = this.props;
     fetchVideo();
+  }
+
+  handlePlayerPlayback(visible) {
+    const { player } = this.state;
+    if (player) {
+      if (visible) {
+        player.playVideo();
+      } else {
+        player.pauseVideo();
+      }
+    }
   }
 
   render() {
@@ -29,7 +46,9 @@ class MusicPage extends Component {
     if (dataState === DataStates.Fetched) {
       return (
         <div className="music">
-          <Youtube videoId={video} opts={opts} />
+          <VisibilitySensor partialVisibility onChange={visible => this.handlePlayerPlayback(visible)}>
+            <Youtube videoId={video} opts={opts} onReady={event => this.setState({ player: event.target })} />
+          </VisibilitySensor>
         </div>
       );
     }
