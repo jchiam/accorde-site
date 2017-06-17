43515ec6603afd2b7df06e2ec7ac0a0cbe2ba2c2
Jonathan Chiam <jczjchiam@gmail.com>
2017-06-17 09:48:34 +0800

    Add hover style to header buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { goToAnchor } from 'react-scrollable-anchor';
 
 export default class Header extends Component {
+  static renderHeaderButton(anchor, label) {
+    return (
+      <button
+        className="header-button"
+        onClick={() => goToAnchor(anchor)}
+      >
+        {label}
+      </button>
+    );
+  }
+
   render() {
     return (
       <div className="header">
-        <button onClick={() => goToAnchor('home')}>Home</button>
-        <button onClick={() => goToAnchor('about')}>About</button>
-        <button onClick={() => goToAnchor('music')}>Music</button>
-        <button onClick={() => goToAnchor('gallery')}>Gallery</button>
-        <button onClick={() => goToAnchor('contact')}>Contact</button>
+        {Header.renderHeaderButton('home', 'Home')}
+        {Header.renderHeaderButton('about', 'About')}
+        {Header.renderHeaderButton('music', 'Music')}
+        {Header.renderHeaderButton('gallery', 'Gallery')}
+        {Header.renderHeaderButton('contact', 'Contact')}
       </div>
     );
   }
