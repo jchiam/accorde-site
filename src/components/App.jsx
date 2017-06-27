968ff38f39873b7c901a942ed8616e1a22ef3d67
Jonathan Chiam <jczjchiam@gmail.com>
2017-06-27 17:31:50 +0800

    Revert "Always show chrome address bar (#48)"
    This reverts commit 4a33e014cd61de7039df570c51f7ff845eacf3e2.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div>
         <Header />
         {this.props.children}
       </div>
