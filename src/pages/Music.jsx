a9a4d234788857f0ad9c16989791298ecff5de6a
Jonathan Chiam <jczjchiam@gmail.com>
2017-08-19 13:23:11 +0800

    Use different youtube embed library for music player

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Youtube from 'react-youtube';
+import ResponsiveEmbed from 'react-responsive-embed';
 
 import PageLoader from 'components/PageLoader';
 import { fetchRandomVideo } from 'actions/youtube';
@@ -10,37 +10,20 @@ import DataStates from 'constants/dataStates';
 import YoutubeIcon from 'images/youtube.svg';
 
 const HEADER_BAR_HEIGHT = 60;
-const YOUTUBE_ASPECT_RATIO = 16 / 9;
 
 class MusicPage extends Component {
-  static generateYoutubeOptions() {
-    const headerHeight = 80;
-    const screenWidth = window.innerWidth - 200;                    // remove 100px as padding buffer
-    const screenHeight = window.innerHeight - headerHeight - 150;   // remove 150px as padding buffer
-    const screenAspectRatio = screenWidth / screenHeight;
+  static generateYoutubeURL(videoID) {
+    return `https://www.youtube.com/embed/${videoID}`;
+  }
 
-    let youtubeWidth;
-    let youtubeHeight;
-
-    // height dependent
-    if (screenAspectRatio > YOUTUBE_ASPECT_RATIO) {
-      youtubeHeight = screenHeight;
-      youtubeWidth = youtubeHeight * YOUTUBE_ASPECT_RATIO;
-    // width dependent
-    } else {
-      youtubeWidth = screenWidth;
-      youtubeHeight = screenWidth / YOUTUBE_ASPECT_RATIO;
-    }
-
-    return {
-      height: youtubeHeight,
-      width: youtubeWidth,
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        color: 'white',
-        rel: 0,
-        showinfo: 0
-      }
-    };
+  static renderPlayer(video) {
+    return (
+      <ResponsiveEmbed
+        src={MusicPage.generateYoutubeURL(video)}
+        ratio="16:9"
+        allowfullscreen
+      />
+    );
   }
 
   constructor(props) {
@@ -58,7 +41,6 @@ class MusicPage extends Component {
   componentWillUnmount() {
     window.removeEventListener('scroll', this.onPlayerPlayback);
   }
-
 
   isVisible() {
     // eslint-disable-next-line react/no-find-dom-node
@@ -84,13 +66,11 @@ class MusicPage extends Component {
 
   render() {
     const { video, title, dataState } = this.props;
-    const opts = MusicPage.generateYoutubeOptions();
-
     return (
       <div className="music">
         <PageLoader loaded={dataState === DataStates.Fetched}>
           <div className="player-title">{title}</div>
-          <Youtube videoId={video} opts={opts} onReady={event => this.setState({ player: event.target })} />
+          {MusicPage.renderPlayer(video)}
           <div className="player-more-info">
             <button onClick={() => window.open(process.env.YOUTUBE_CHANNEL)}>
               Visit our YouTube page
