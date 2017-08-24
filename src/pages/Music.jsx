1a57771bd83732c6ca5587d6a40cc60182dbd5c8
Jonathan Chiam <jczjchiam@gmail.com>
2017-08-24 11:29:51 +0800

    Make youtube player responsive

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -10,31 +10,12 @@ import DataStates from 'constants/dataStates';
 import YoutubeIcon from 'images/youtube.svg';
 
 const HEADER_BAR_HEIGHT = 60;
-const YOUTUBE_ASPECT_RATIO = 16 / 9;
 
 class MusicPage extends Component {
   static generateYoutubeOptions() {
-    const headerHeight = 80;
-    const screenWidth = window.innerWidth - 200;                    // remove 100px as padding buffer
-    const screenHeight = window.innerHeight - headerHeight - 150;   // remove 150px as padding buffer
-    const screenAspectRatio = screenWidth / screenHeight;
-
-    let youtubeWidth;
-    let youtubeHeight;
-
-    // height dependent
-    if (screenAspectRatio > YOUTUBE_ASPECT_RATIO) {
-      youtubeHeight = screenHeight;
-      youtubeWidth = youtubeHeight * YOUTUBE_ASPECT_RATIO;
-    // width dependent
-    } else {
-      youtubeWidth = screenWidth;
-      youtubeHeight = screenWidth / YOUTUBE_ASPECT_RATIO;
-    }
-
     return {
-      height: youtubeHeight,
-      width: youtubeWidth,
+      height: 1600,
+      width: 900,
       playerVars: { // https://developers.google.com/youtube/player_parameters
         color: 'white',
         rel: 0,
@@ -90,7 +71,9 @@ class MusicPage extends Component {
       <div className="music">
         <PageLoader loaded={dataState === DataStates.Fetched}>
           <div className="player-title">{title}</div>
-          <Youtube videoId={video} opts={opts} onReady={event => this.setState({ player: event.target })} />
+          <div className="player-container">
+            <Youtube videoId={video} opts={opts} onReady={event => this.setState({ player: event.target })} />
+          </div>
           <div className="player-more-info">
             <button onClick={() => window.open(process.env.YOUTUBE_CHANNEL)}>
               Visit our YouTube page
