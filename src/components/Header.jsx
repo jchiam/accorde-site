4b7248d598b02982753fec7eec3ebb66b5063d86
Jonathan Chiam <jczjchiam@gmail.com>
2017-09-04 15:31:12 +0800

    Use react-scroll instead of react-scrollable-anchor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,62 +1,40 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import { goToAnchor, removeHash } from 'react-scrollable-anchor';
+import Scroll from 'react-scroll';
 
-const HEADER_STATES = {
-  HOME: 'home',
-  ABOUT: 'about',
-  MUSIC: 'music',
-  GALLERY: 'gallery',
-  CONTACT: 'contact'
-};
+const Link = Scroll.Link;
+const scroll = Scroll.animateScroll;
+const SCROLL_OFFSET = -60;
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { header: '' };
-    this.onHashChange = this.handleHashChange.bind(this);
-  }
-
-  componentWillMount() {
-    this.setState({ header: window.location.hash ? window.location.hash.substring(1) : HEADER_STATES.HOME });
+  static renderHeaderButton(anchor, label) {
+    return (
+      <Link
+        className="header-button"
+        activeClass="header-button-selected"
+        to={anchor}
+        offset={SCROLL_OFFSET}
+        spy
+        smooth
+        isDynamic
+      >
+        {label}
+      </Link>
+    );
   }
 
   componentDidMount() {
-    window.addEventListener('hashchange', this.onHashChange);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('hashchange', this.onHashChange);
-  }
-
-  handleHashChange() {
-    this.setState({ header: window.location.hash.substring(1) });
-  }
-
-  renderHeaderButton(anchor, label) {
-    const { header } = this.state;
-    return (
-      <button
-        className={classNames({
-          'header-button': true,
-          'header-button-selected': header === anchor
-        })}
-        onClick={() => { removeHash(); goToAnchor(anchor); }}
-      >
-        {label}
-      </button>
-    );
+    scroll.scrollToTop(); // set initial scroll position to top
   }
 
   render() {
     return (
       <div className="header-container">
         <div className="header">
-          {this.renderHeaderButton('home', 'HOME')}
-          {this.renderHeaderButton('about', 'ABOUT')}
-          {this.renderHeaderButton('music', 'MUSIC')}
-          {this.renderHeaderButton('gallery', 'GALLERY')}
-          {this.renderHeaderButton('contact', 'CONTACT')}
+          {Header.renderHeaderButton('home', 'HOME')}
+          {Header.renderHeaderButton('about', 'ABOUT')}
+          {Header.renderHeaderButton('music', 'MUSIC')}
+          {Header.renderHeaderButton('gallery', 'GALLERY')}
+          {Header.renderHeaderButton('contact', 'CONTACT')}
         </div>
       </div>
     );
