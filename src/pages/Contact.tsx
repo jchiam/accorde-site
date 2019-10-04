2abd1ae77b61a2c854a7a2f9273739292af4640c
Jonathan Chiam <jczjchiam@gmail.com>
2019-10-04 19:03:03 +0800

    Load contact page svg icons as image (#95)
    * Extract and sanitise svg files
    
    * Replace component icons with svg icons

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 
-import YoutubeIcon from 'components/YoutubeIcon';
-import FacebookIcon from 'components/FacebookIcon';
-import EmailIcon from 'components/EmailIcon';
-
 export default class Contact extends Component {
+  renderYoutubeButton() {
+    return (
+      <div className="social-media">
+        <button
+          className="social-media-icon"
+          onClick={() => window.open(process.env.YOUTUBE_CHANNEL)}
+        >
+          <img src={require('images/youtube-contact.svg')} />
+        </button>
+        Visit Our<br />Youtube Channel
+      </div>
+    );
+  }
+
+  renderFacebookButton() {
+    return (
+      <div className="social-media">
+        <button
+          className="social-media-icon"
+          onClick={() => window.open(process.env.FACEBOOK_PAGE)}
+        >
+          <img src={require('images/facebook-contact.svg')} />
+        </button>
+        Visit Our<br />Facebook Page
+      </div>
+    );
+  }
+
+  renderEmailButton() {
+    return (
+      <div className="social-media">
+        <a className="social-media-icon" href={`mailto:${process.env.EMAIL}`}>
+          <img src={require('images/email-contact.svg')} />
+        </a>
+        Email Us!
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="contact">
         <div className="title">GET IN TOUCH!</div>
         <div className="social-media-container">
-          <div className="social-media">
-            <YoutubeIcon className="social-media-icon" onClick={() => window.open(process.env.YOUTUBE_CHANNEL)} />
-            Visit Our<br />Youtube Channel
-          </div>
-          <div className="social-media">
-            <FacebookIcon className="social-media-icon" onClick={() => window.open(process.env.FACEBOOK_PAGE)} />
-            Visit Our<br />Facebook Page
-          </div>
-          <div className="social-media">
-            <a href={`mailto:${process.env.EMAIL}`}>
-              <EmailIcon className="social-media-icon" />
-            </a>
-            Email Us!
-          </div>
+          {this.renderYoutubeButton()}
+          {this.renderFacebookButton()}
+          {this.renderEmailButton()}
         </div>
         <div className="footer">Recruitment // Engagement // Outreach</div>
       </div>
